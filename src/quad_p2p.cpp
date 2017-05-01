40db07d505329e7a25904ca276b6a8a626f52a11
amanchandra333 <amanchandra333@gmail.com>
2017-05-01 19:40:36 +0530

    added test code for library

--- src/quad_p2p.cpp
+++ src/quad_p2p.cpp
@@ -1,18 +1,10 @@
-#include <ros/ros.h>
-#include <math.h>
-#include <std_msgs/String.h>
+#include "ark_llp/go2goal.h"
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "quad_p2p");
-    ros::Publisher go2goal = n.advertise<std_msgs::String>("/tum_ardrone/com", 10);
-    ros::NodeHandle n;
-    ros::Rate loop_rate(10);
+    Go2Goal random;
+    random.set_dest();
 
-    while (ros::ok()){
-      ros::spinOnce();
-      loop_rate.sleep();
-    }
-    
     return 0;
 }
