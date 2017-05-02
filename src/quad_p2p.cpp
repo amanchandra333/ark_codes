76df0cccbc5fe392fc5b30b9c6f1b92b2c91a011
amanchandra333 <amanchandra333@gmail.com>
2017-05-02 22:59:12 +0530

    added function for adding dest

--- src/quad_p2p.cpp
+++ src/quad_p2p.cpp
@@ -1,10 +1,19 @@
 #include "ark_llp/go2goal.h"
-
+#include <ros/ros.h>
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "quad_p2p");
     Go2Goal random;
-    random.set_dest();
+    float x, y, z, theta, func;
+    x = std::atof(argv[1]);
+    y = std::atof(argv[2]);
+    z = std::atof(argv[3]);
+    theta = std::atof(argv[4]);
+    func = std::atof(argv[5]);
+    if(func==1)
+    	random.set_dest(x, y, z, theta);
+    else
+    	random.add_dest(x, y, z, theta);
 
     return 0;
 }
